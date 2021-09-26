83da376682ca0e8e3bd5263d26837f136a6b5f28
Feedbaek <nicek789@naver.com>
2021-09-26 21:19:16 +0900

    21.09.26

--- 42_agenda/42.py
+++ 42_agenda/42.py
@@ -6,7 +6,15 @@
 #    By: minskim2 <minskim2@student.42seoul.kr>     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/09/26 17:30:03 by minskim2          #+#    #+#              #
-#    Updated: 2021/09/26 17:36:11 by minskim2         ###   ########.fr        #
+#    Updated: 2021/09/26 19:48:58 by minskim2         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+import pyautogui
+import time
+
+while 1:
+	pyautogui.click(x=600,y=400)
+	time.sleep(0.1)
+	pyautogui.doubleClick(x=650,y=450)
+	time.sleep(0.1)
