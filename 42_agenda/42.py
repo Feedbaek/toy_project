16ff133b980b4db95f3030798be85482637ad963
Feedbaek <nicek789@naver.com>
2021-09-27 22:28:53 +0900

    21.09.27

--- 42_agenda/42.py
+++ 42_agenda/42.py
@@ -6,15 +6,16 @@
 #    By: minskim2 <minskim2@student.42seoul.kr>     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/09/26 17:30:03 by minskim2          #+#    #+#              #
-#    Updated: 2021/09/26 19:48:58 by minskim2         ###   ########.fr        #
+#    Updated: 2021/09/27 22:28:47 by minskim2         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import pyautogui
+import keyboard
 import time
 
 while 1:
-	pyautogui.click(x=600,y=400)
-	time.sleep(0.1)
-	pyautogui.doubleClick(x=650,y=450)
-	time.sleep(0.1)
+		position = pyautogui.position()
+		if keyboard.is_pressed('enter'):
+				print(position)
+				time.sleep(0.2)
